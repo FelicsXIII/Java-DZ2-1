cd22cc8009d570880f4ff41c774cdc735908df40
FelicsXIII <felics1689@gmail.com>
2023-11-10 14:17:53 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Программа расчета милей");
-        int x = 20; //  за каждые 20 рублей начисляется 1 миля
-        int amout = 5_555; // цена билета
-        System.out.println("Cумма покупки " +  amout + " рублей");
-        int bonus = amout / x;
-        System.out.println("Начислено:" + bonus + " милей");
+        int formula = 20; //  за каждые 20 рублей начисляется 1 миля
+        int amout = 1898323; // цена билета
+        System.out.println("Cумма покупки: " +  amout + " рублей");
+        int bonus = amout / formula;
+        System.out.println("Начислено: " + bonus + " милей");
     }
 }
